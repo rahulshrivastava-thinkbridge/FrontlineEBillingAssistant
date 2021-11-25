17f8c05711f319fb24d32785dcf49610c6540543
Rahul Shrivastava <rahul.shrivastava@thinkbridge.in>
2021-11-25 17:33:07 +0530

    Modify line items api issues fixed

--- FrontlineEBillingAssistant.Repository/Repository/InvoiceLineItemGridRepository.cs
+++ FrontlineEBillingAssistant.Repository/Repository/InvoiceLineItemGridRepository.cs
@@ -5,7 +5,7 @@ using FrontlineEBillingAssistant.Repository.Repository.Base;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,7 +21,7 @@ namespace FrontlineEBillingAssistant.Repository.Repository
             {
                 return await _ebillingContext.InvoiceLineItemsListingGrid.FromSqlRaw<InvoiceLineItemsListingGridModel>("GetInvoiceLineItemsById {0}", Id).ToListAsync();
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return new List<InvoiceLineItemsListingGridModel>();
             }
@@ -37,26 +37,26 @@ namespace FrontlineEBillingAssistant.Repository.Repository
             {
                 param = new List<SqlParameter>
                 {
-                    new SqlParameter { ParameterName = "@Id", Value = model.LineItemId },
-                    new SqlParameter { ParameterName = "@RuleCode", Value = model.Rule },
-                    new SqlParameter { ParameterName = "@Units", Value = model.Units },
-                    new SqlParameter { ParameterName = "@Rate", Value = model.Rate },
-                    new SqlParameter { ParameterName = "@AgreedRate", Value = model.AgreedRate },
-                    new SqlParameter { ParameterName = "@Discounts", Value = model.Discounts },
-                    new SqlParameter { ParameterName = "@Total", Value = model.Total },
-                    new SqlParameter { ParameterName = "@Tags", Value = model.Tags },
-                    new SqlParameter { ParameterName = "@ChargeDate", Value = model.Date },
-                    new SqlParameter { ParameterName = "@TaskCode", Value = model.Task },
-                    new SqlParameter { ParameterName = "@TaskActivityCode", Value = model.Activity },
-                    new SqlParameter { ParameterName = "@TimeKeeperInitial", Value = model.TimekeeperInitial },
-                    new SqlParameter { ParameterName = "@ChargeDesciption", Value = model.Description },
-                    new SqlParameter { ParameterName = "@PreparationNotes", Value = model.PreperationNotes },
-                    new SqlParameter { ParameterName = "@MLPreparationNotes", Value = model.MLPreparationNotes }
+                    new SqlParameter { ParameterName = "@Id", Value = model.LineItemId, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@RuleCode", Value = string.IsNullOrEmpty(model.Rule) ? DBNull.Value : model.Rule, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@Units", Value = model.Units.Equals(null) ? 0 : model.Units, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@Rate", Value = string.IsNullOrEmpty(model.Rate) ? DBNull.Value : model.Rate, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@AgreedRate", Value = string.IsNullOrEmpty(model.AgreedRate) ? DBNull.Value : model.AgreedRate, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@Discounts", Value = string.IsNullOrEmpty(model.Discounts) ? DBNull.Value : model.Discounts, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@Total", Value = string.IsNullOrEmpty(model.Total) ? DBNull.Value : model.Total, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@Tags", Value = string.IsNullOrEmpty(model.Tags) ? DBNull.Value : model.Tags, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@ChargeDate", Value = string.IsNullOrEmpty(model.Date) ? DBNull.Value : model.Date, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@TaskCode", Value = string.IsNullOrEmpty(model.Task) ? DBNull.Value : model.Task, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@TaskActivityCode", Value = string.IsNullOrEmpty(model.Activity) ? DBNull.Value : model.Activity, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@TimeKeeperInitial", Value = string.IsNullOrEmpty(model.TimekeeperInitial) ? DBNull.Value : model.TimekeeperInitial, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@ChargeDesciption", Value = string.IsNullOrEmpty(model.Description) ? DBNull.Value : model.Description, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@PreparationNotes", Value = string.IsNullOrEmpty(model.PreperationNotes) ? "..." : model.PreperationNotes, Direction = System.Data.ParameterDirection.Input },
+                    new SqlParameter { ParameterName = "@MLPreparationNotes", Value = string.IsNullOrEmpty(model.MLPreparationNotes) ? "..." : model.MLPreparationNotes, Direction = System.Data.ParameterDirection.Input }
                 };
 
                 try
                 {
-                    rowsAffected += _ebillingContext.Database.ExecuteSqlRaw(sql, param.ToArray());
+                    rowsAffected += _ebillingContext.Database.ExecuteSqlRaw(sql, param);
                 }
                 catch (Exception)
                 {
